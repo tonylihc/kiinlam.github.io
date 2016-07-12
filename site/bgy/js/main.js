403aaa6c95d9499f75eec5ccda00278a831f8d08
kiinlam <kiinlam.cheung@gmail.com>
2016-07-12 11:58:02 +0800

    update tutorial

--- site/bgy/js/main.js
+++ site/bgy/js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+	var scrollWidth = Math.max(document.documentElement.scrollWidth, document.body.scrollWidth),
+		scrollItem = $('.scroll-h b'),
+		w = scrollItem.width();
+		timeout = null,
+		left = scrollWidth,
+		step = 3,
+		duration = 50;
+
+	function move () {
+		left -= step;
+		if (left <= -1*w) {
+			left = scrollWidth;
+		}
+		scrollItem.css('left', left + 'px');
+
+		setTimeout(move, duration);
+	}
+
+	setTimeout(move, duration);
 
 });
